f783b19be184057f57406c7a0db019a0b27598fe
Connor Petri <cpetri@printf.org>
2025-08-28 09:13:36 -0700

    Lab 2

---
+++ java_programming/labs/gathering_input/GatheringInput.java
@@ -0,0 +1,19 @@
+package java_programming.labs.gathering_input;
+
+import java.util.Scanner;
+
+public class GatheringInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input U.S. Dollar currency: $");
+        float dollars = scanner.nextFloat();
+        
+        System.out.print("Input Exchange Rate: ");
+        float exchangeRate = scanner.nextFloat();
+
+        System.out.print("Target Currency: ");
+        float finalAmount = Math.round((dollars * exchangeRate) * 100.0f) / 100.0f;
+        System.out.println(String.format("%.2f", finalAmount));
+    }
+}
