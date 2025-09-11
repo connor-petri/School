57ac7f179d5da675b331ab96f50d32de0081f029
Connor Petri <cpetri@printf.org>
2025-09-11 09:33:52 -0700

    books lab

---
+++ java_programming/labs/books/Driver.java
@@ -0,0 +1,24 @@
+package java_programming.labs.books;
+import java.util.*;
+
+public class Driver {
+    static ArrayList<Book> arr = new ArrayList<Book>();
+
+    public static void main(String[] args) {
+        try (Scanner s = new Scanner(System.in)) {
+            for (int i = 0; i < 3; i++) {
+                System.out.print("Book " + (i+1) + " Title: ");
+                String title = s.nextLine();
+                System.out.print("Book " + (i+1) + " Author: ");
+                String author = s.nextLine();
+
+                arr.add(new Book(title, author));
+            }
+        }
+
+        System.out.println("Your Library:");
+        for (Book b : arr) {
+            System.out.println(b.getTitle() + " - " + b.getAuthor());
+        }
+    }
+}
